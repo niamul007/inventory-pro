9435545526165db1cbee7586faadb94b6e36e813
niamul007 <am3994566@gmail.com>
2026-02-15 22:47:04 +0600

    adding server but test will be tomorrowe

---
+++ server/src/server.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'node:path';
+import router from './routes/routes.mjs';
+import AppError from './utilities/appError.mjs';
+import globalErrorHandle from './middleware/errorMiddleware.mjs';
+
+
+const __dirname = import.meta.dirname;
+const app = express();
+const PORT = 7001;
+const filePath = path.join(__dirname, "..", "public");
+
+app.use(express.json());
+app.use(express.static(filePath));
+app.use("/api", router);
+app.use((req, res, next) => {
+    next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+})
+app.use(globalErrorHandle);
+
+app.listen(PORT, () => {
+    console.log(`server running on : http://localhost:${PORT}`);
+
+})
