bc3bca473a0d0eddba5e8110a2861633db4acd59
niamul007 <am3994566@gmail.com>
2026-02-16 22:18:48 +0600

    adding new fix code

--- server/src/controller/productController.mjs
+++ server/src/controller/productController.mjs
@@ -1,29 +1,19 @@
-// 1. Double check your path (usually it's '../services/...' if in the controllers folder)
 import * as productService from "../service/productService.mjs";
-import { catchAsync } from "../utils/catchAsync.mjs";
+import catchAsync from "../utilities/catchAsync.mjs";
 
 export const getProduct = catchAsync(async (req, res, next) => {
-  // 2. Call the service
   const products = await productService.getAllProducts();
-
-  // 3. Send the response
   res.status(200).json({
     status: "success",
-    results: products.length, // Senior Tip: Always send the count for arrays
-    data: {
-      products, // Wrapping in an object makes it easier to add more data later
-    },
+    results: products.length,
+    data: { products },
   });
 });
 
 export const addProduct = catchAsync(async (req, res, next) => {
-  const products = await productService.createProduct(req.body);
-
-  res.status(200).json({
+  const product = await productService.createProduct(req.body);
+  res.status(201).json({
     status: "success",
-    results: product.length,
-    data: {
-      products,
-    },
+    data: { product },
   });
 });
