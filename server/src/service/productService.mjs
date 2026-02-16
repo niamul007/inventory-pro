ac3066a23c544a94769d2d07dacadea3861650be
niamul007 <am3994566@gmail.com>
2026-02-16 22:19:33 +0600

    adding working code

--- server/src/service/productService.mjs
+++ server/src/service/productService.mjs
@@ -1,32 +1,17 @@
 import { pool } from '../config/db.mjs';
 
-/**
- * We use 'async' because talking to a database takes time.
- * We use 'await' to wait for the database to reply before moving to the next line.
- */
 export const getAllProducts = async () => {
-    // 1. The SQL Command
     const sql = 'SELECT * FROM products ORDER BY created_at DESC';
-
-    // 2. The Execution
-    // This is the "Aha!" moment: we wait for the pool to finish the query
     const result = await pool.query(sql);
-
-    // 3. The Data
-    // 'pg' always returns an object. The actual rows of data are inside '.rows'
     return result.rows; 
 };
 
-
 export const createProduct = async (productData) => {
-    const { name, category, price, stock_quantity } = productData;
-    
-    // $1, $2, etc. are placeholders to prevent SQL Injection
+    const { name, category, price, stock } = productData;
     const sql = `
-        INSERT INTO products (name, category, price, stock_quantity) 
+        INSERT INTO products (name, category, price, stock) 
         VALUES ($1, $2, $3, $4) 
         RETURNING *`;
-        
-    const { rows } = await pool.query(sql, [name, category, price, stock_quantity]);
+    const { rows } = await pool.query(sql, [name, category, price, stock]);
     return rows[0];
 };
