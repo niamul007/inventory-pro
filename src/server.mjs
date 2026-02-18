f35829b5a0cfcdb3709bf0a219e556f5e7f38449
niamul007 <am3994566@gmail.com>
2026-02-18 21:04:12 +0600

    adding new methods for database

--- src/server.mjs
+++ src/server.mjs
@@ -3,16 +3,23 @@ import path from 'node:path';
 import router from './routes/routes.mjs';
 import AppError from './utilities/appError.mjs';
 import globalErrorHandle from './middleware/errorMiddleware.mjs';
+import cors from 'cors';
 
 
 const __dirname = import.meta.dirname;
 const app = express();
 const PORT = 7001;
-const filePath = path.join(__dirname, "..", "public");
+const filePath = path.join(__dirname, "../client/project");
+console.log(filePath)
 
 app.use(express.json());
 app.use(express.static(filePath));
 app.use("/api", router);
+app.use(cors())
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/project/index.html"));
+});
 app.use((req, res, next) => {
     next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
 })
