a68c73a1a63520074b6ed9ee75eec99755f904e4
niamul007 <am3994566@gmail.com>
2026-02-18 22:44:33 +0600

    adding cors

--- server/src/config/db.mjs
+++ server/src/config/db.mjs
@@ -6,7 +6,7 @@ const poolConfig = {
   user: 'neondb_owner',
   host: 'ep-bold-cake-a1jg4nz0-pooler.ap-southeast-1.aws.neon.tech',
   database: 'neondb',
-  password: 'npg_AjnHPv48tCzG', // <--- TYPE YOUR PASSWORD HERE
+  password: '', // <--- TYPE YOUR PASSWORD HERE
   port: 5432,
   ssl: {
     rejectUnauthorized: false, // Required for Neon
