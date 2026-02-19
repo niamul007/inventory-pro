cb132ec53ac677da9cb299f6c401e0d6adead6ac
niamul007 <am3994566@gmail.com>
2026-02-19 17:39:55 +0600

    adind del fn

--- src/service/productService.mjs
+++ src/service/productService.mjs
@@ -15,14 +15,3 @@ export const createProduct = async (productData) => {
     const { rows } = await pool.query(sql, [name, category, price, stock]);
     return rows[0];
 };
-
-export const delProduct = async (id) => {
-    // 1. Use the DELETE command
-    // 2. Use $1 as a placeholder to prevent SQL Injection
-    const sql = `DELETE FROM products WHERE id = $1 RETURNING *`;    
-    // 3. Pass the ID in an array as the second argument
-    const result = await pool.query(sql, [id]);
-    
-    // Return the deleted item so the frontend knows it's gone
-    return result.rows[0];
-};
