93135c2d8be25f48af3ef74566078b899488b49f
niamul007 <am3994566@gmail.com>
2026-02-20 22:14:22 +0600

    addding put data

--- server/src/service/productService.mjs
+++ server/src/service/productService.mjs
@@ -22,3 +22,17 @@ export const delProduct = async (id) =>{
     const result =  await pool.query(sql,[id]);
     return result.rows[0];
 }
+
+export const updateProduct = async (id, productData) => {
+    const { name, category, price, stock } = productData;
+    
+    // Remove "updated_at" from this string!
+    const sql = `
+        UPDATE products 
+        SET name = $1, category = $2, price = $3, stock = $4
+        WHERE id = $5 
+        RETURNING *`;
+
+    const { rows } = await pool.query(sql, [name, category, price, stock, id]);
+    return rows[0];
+};
