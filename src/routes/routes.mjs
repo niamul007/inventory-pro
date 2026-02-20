b73a9e757a69688b3ea8bf20fda63ab3fd8562c3
niamul007 <am3994566@gmail.com>
2026-02-20 17:19:21 +0600

    adding del route

--- src/routes/routes.mjs
+++ src/routes/routes.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import { validate } from "../middleware/validate.mjs";
 import productSchema from "../middleware/validateProduct.mjs";
-import { getProduct, addProduct } from "../controller/productController.mjs";
+import { getProduct, addProduct , delProduct } from "../controller/productController.mjs";
 
 const router = express.Router();
 router.get("/test", (req, res) => {
@@ -10,5 +10,6 @@ router.get("/test", (req, res) => {
 router.get("/get-product", getProduct);
 // Validation comes FIRST, then addProduct
 router.post("/add-product", validate(productSchema), addProduct);
+router.delete("/delete-product/:id", delProduct)
 
 export default router;
