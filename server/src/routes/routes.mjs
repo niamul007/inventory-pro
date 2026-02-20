ec66dbf072540ed3186f74acaf8dfcad1864e867
niamul007 <am3994566@gmail.com>
2026-02-20 22:13:38 +0600

    add update routez

--- server/src/routes/routes.mjs
+++ server/src/routes/routes.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import { validate } from "../middleware/validate.mjs";
 import productSchema from "../middleware/validateProduct.mjs";
-import { getProduct, addProduct , delProduct } from "../controller/productController.mjs";
+import { getProduct, addProduct , delProduct , updateProduct } from "../controller/productController.mjs";
 
 const router = express.Router();
 router.get("/test", (req, res) => {
@@ -11,5 +11,5 @@ router.get("/get-product", getProduct);
 // Validation comes FIRST, then addProduct
 router.post("/add-product", validate(productSchema), addProduct);
 router.delete("/delete-product/:id", delProduct)
-
+router.put("/update-product/:id", updateProduct); // IS THIS LINE EXACTLY HERE?
 export default router;
